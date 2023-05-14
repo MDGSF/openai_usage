247cd3e8d0f525235dfbada5cb29f02e91c3c63f
HuangJian <1342042894@qq.com>
2023-05-14 20:10:06 +0800

    update demo 03

--- openai_demo_03.py
+++ openai_demo_03.py
@@ -18,11 +18,33 @@ def get_completion(prompt, model='gpt-3.5-turbo'):
     return response.choices[0].message["content"]
 
 
+text_1 = f"""
+Making a cup of tea is easy! First, you need to get some \
+water boiling. While that's happening, \
+grab a cup and put a tea bag in it. Once the water is \
+hot enough, just pour it over the tea bag. \
+Let it sit for a bit so the tea can steep. After a \
+few minutes, take out the tea bag. If you \
+like, you can add some sugar or milk to taste. \
+and that's it! you've got yourself a delicious \
+cup of tea to enjoy.
+"""
+
 prompt = f"""
-Generate a list of three made-up book titles along \
-with their authors and genres.
-Privide them in JSON format with the following keys:
-book_id, title, author, genre.
+You will be provided with text delimited by triple quotes.
+If it contains a sequences of instructions, \
+re-write those instructions in the following format:
+
+Step 1 = ...
+Step 2 = ...
+...
+Step N = ...
+
+If the text does not contain a sequences of instructions, \
+then simply write \"No steps privided.\"
+
+\"\"\"{text_1}\"\"\"
 """
 response = get_completion(prompt)
-print (response)
+print("Completion for Text 1:")
+print(response)
