9229f4ea85e7a664b1ecce2a7b72e6b267959b5b
HuangJian <1342042894@qq.com>
2023-05-18 22:25:24 +0800

    update demo08

--- openai_demo_08.py
+++ openai_demo_08.py
@@ -18,9 +18,40 @@ def get_completion(prompt, model='gpt-3.5-turbo'):
     return response.choices[0].message["content"]
 
 prompt = f"""
-Determine if the student's solution is correct or not.
+Your task is to determine if the student's solution \
+is correct or not.
+To solve the problem do the following:
+- First, work out your own solution to the problem.
+- Then compare your solution to the student's solution \
+and evaluate if the student's solution is correct or not.
+Don't decide if the student's solution is correct until
+you have done the problem yourself.
+
+Use the following format:
+Question:
+```
+question here
+```
+Student's solution:
+```
+student's solution here
+```
+Actual solution:
+```
+steps to work out the solution and your solution here
+```
+Is the student's solution the same as the actual solution \
+just calculated:
+```
+yes or no
+```
+student grade:
+```
+correct or incorrect
+```
 
 Question:
+```
 I'm building a solar power installation and I need \
 help working out the financials.
 - Land costs $100 / square foot
@@ -30,14 +61,17 @@ me a flat $100k per year, and an additional $10 / square \
 foot
 What is the total cost for the first year of operations
 as a function of the number of square feet.
-
+```
 Student's Solution:
+```
 Let x be the size of the installation in square feet.
 Costs:
 1. Land cost: 100x
 2. Solar panel cost: 250x
 3. Maintenance cost: 100,00 + 100x
 Total cost: 100x + 250x + 100,000 + 100x = 450x + 100,000
+```
+Actual solution:
 """
 
 # 这个提示词这么写，会出错
